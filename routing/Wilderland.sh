86603925ed7c0e385a28048a37dac99d81e05827
reva <reva.erica24@gmail.com>
2025-12-03 20:33:52 +0700

    Update Wilderland.sh

--- routing/Wilderland.sh
+++ routing/Wilderland.sh
@@ -1,2 +1,9 @@
-route add -net 0.0.0.0/0 gw 10.88.2.221  # Default via Moria
-route add -net 10.88.2.192/29 gw 10.88.2.230  # Khamul via Switch2
+# Default route ke Moria
+route add -net 0.0.0.0/0 gw 10.88.2.221
+
+# Client/server networks via Moria
+route add -net 10.88.0.0/24 gw 10.88.2.210
+route add -net 10.88.2.0/25 gw 10.88.2.210
+route add -net 10.88.2.200/29 gw 10.88.2.210
+route add -net 10.88.2.224/30 gw 10.88.2.210
+route add -net 10.88.2.244/30 gw 10.88.2.210
