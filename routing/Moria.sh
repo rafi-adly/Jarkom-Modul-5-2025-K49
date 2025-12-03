0be077258e764d13039c127f102a303f4e6aeddb
reva <reva.erica24@gmail.com>
2025-12-03 20:20:26 +0700

    Delete routing/Moria.sh

--- routing/Moria.sh
+++
@@ -1,3 +0,0 @@
-route add -net 0.0.0.0/0 gw 10.88.2.209  # Default via Osgiliath
-route add -net 10.88.2.128/26 gw 10.88.2.222  # Durin via Wilderland
-route add -net 10.88.2.192/29 gw 10.88.2.222  # Khamul via Wilderland
