b0525fd58a019022f3f380941b8341d3887d3a02
reva <reva.erica24@gmail.com>
2025-12-04 23:25:20 +0700

    Update Osgiliath.sh

--- routing/Osgiliath.sh
+++ routing/Osgiliath.sh
@@ -1,20 +1,19 @@
-
 #!/bin/bash
 
-# Via Minastir (eth1: 10.88.2.217)
-ip route add 10.88.0.0/24 via 10.88.2.218  
-ip route add 10.88.2.0/25 via 10.88.2.218  
-ip route add 10.88.2.240/30 via 10.88.2.218  
-ip route add 10.88.2.244/30 via 10.88.2.218  
-ip route add 10.88.2.248/30 via 10.88.2.218 
+echo "[+] Setting static routes for Osgiliath..."
 
-# Via Moria (eth2: 10.88.2.209)
-ip route add 10.88.2.128/26 via 10.88.2.210 
-ip route add 10.88.2.192/29 via 10.88.2.210 
-ip route add 10.88.2.220/30 via 10.88.2.210 
-ip route add 10.88.2.224/30 via 10.88.2.210    
+ip route add 10.88.0.0/24 via 10.88.2.218
+ip route add 10.88.2.0/25 via 10.88.2.218
+ip route add 10.88.2.240/30 via 10.88.2.218
+ip route add 10.88.2.244/30 via 10.88.2.218
+ip route add 10.88.2.248/30 via 10.88.2.218
+ip route add 10.88.2.236/30 via 10.88.2.218
 
-# Via Rivendell (eth3: 10.88.2.213)
-ip route add 10.88.2.200/29 via 10.88.2.214 
+ip route add 10.88.2.128/26 via 10.88.2.210
+ip route add 10.88.2.192/29 via 10.88.2.210
+ip route add 10.88.2.220/30 via 10.88.2.210
+ip route add 10.88.2.224/30 via 10.88.2.210
 
-echo "Routing configured!"
+ip route add 10.88.2.200/29 via 10.88.2.214
+
+echo "[✓] Routing Osgiliath COMPLETE!"
