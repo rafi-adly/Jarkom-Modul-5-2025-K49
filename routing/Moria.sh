cdeb97a6628914ff16d0c54ef7c5a72d903dfa01
reva <reva.erica24@gmail.com>
2025-12-04 23:47:35 +0700

    Update Moria.sh

--- routing/Moria.sh
+++ routing/Moria.sh
@@ -1,17 +1,4 @@
-#!/bin/bash
+route add -net 10.88.2.128/26 gw 10.88.2.222
+route add -net 10.88.2.192/29 gw 10.88.2.222
 
-echo "[MORIA] Configuring networking..."
-
-# IP Forwarding
-echo 1 > /proc/sys/net/ipv4/ip_forward
-
-# Default route ke Osgiliath
-ip route add default via 10.88.2.209 dev eth0
-
-# Route ke Durin via Wilderland
-ip route add 10.88.2.128/26 via 10.88.2.222 dev eth1
-
-# Route ke Khamul via Wilderland
-ip route add 10.88.2.192/29 via 10.88.2.222 dev eth1
-
-echo "[✓] Moria routing configured!"
+echo "nameserver 192.168.122.1" > /etc/resolv.conf
