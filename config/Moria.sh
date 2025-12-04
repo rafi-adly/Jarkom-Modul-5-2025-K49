8be3cac55b4b17835debd701e4f0f502585ad599
reva <reva.erica24@gmail.com>
2025-12-04 22:46:27 +0700

    Update Moria.sh

--- config/Moria.sh
+++ config/Moria.sh
@@ -1,16 +1,20 @@
+auto lo
+iface lo inet loopback
+
 auto eth0
 iface eth0 inet static
-    address 10.88.2.225
+    address 10.88.2.210
     netmask 255.255.255.252
+    gateway 10.88.2.209
 
 auto eth1
 iface eth1 inet static
-    address 10.88.2.221
+    address 10.88.2.225
     netmask 255.255.255.252
 
 auto eth2
 iface eth2 inet static
-    address 10.88.2.210
+    address 10.88.2.221
     netmask 255.255.255.252
 
 # Routing
