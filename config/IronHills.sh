3e16db6816dfd495a9a2b4aa984534ebb935f9ab
reva <reva.erica24@gmail.com>
2025-12-05 08:55:17 +0700

    Update IronHills.sh

--- config/IronHills.sh
+++ config/IronHills.sh
@@ -1,7 +1,10 @@
+auto lo
+iface lo inet loopback
+
 auto eth0
 iface eth0 inet static
-    address 10.88.2.205
-    netmask 255.255.255.248
-    gateway 10.88.2.201
+    address 10.88.2.226
+    netmask 255.255.255.252
+    gateway 10.88.2.225
 
-up echo "nameserver 192.168.122.1" > /etc/resolv.conf
+up echo "nameserver 10.88.2.203" > /etc/resolv.conf
