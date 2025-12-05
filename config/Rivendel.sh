8872f18b1b91e0b0f028143af6ccc89b97bcc072
reva <reva.erica24@gmail.com>
2025-12-05 09:23:58 +0700

    Update Rivendel.sh

--- config/Rivendel.sh
+++ config/Rivendel.sh
@@ -1,10 +1,13 @@
+auto lo
+iface lo inet loopback
+
 auto eth0
 iface eth0 inet static
-    address 10.88.2.214  
+    address 10.88.2.214
     netmask 255.255.255.252
     gateway 10.88.2.213
 
 auto eth1
 iface eth1 inet static
-    address 10.88.2.201    
+    address 10.88.2.201
     netmask 255.255.255.248
