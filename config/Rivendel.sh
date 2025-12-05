3187ee632628ab784130914fcca70c2e1bb8766c
reva <reva.erica24@gmail.com>
2025-12-05 08:38:42 +0700

    Update Rivendel.sh

--- config/Rivendel.sh
+++ config/Rivendel.sh
@@ -1,13 +1,10 @@
-auto eth1
-iface eth1 inet static
-    address 10.88.2.214
+auto eth0
+iface eth0 inet static
+    address 10.88.2.214  
     netmask 255.255.255.252
     gateway 10.88.2.213
 
-auto eth0
-iface eth0 inet static
-    address 10.88.2.201
+auto eth1
+iface eth1 inet static
+    address 10.88.2.201    
     netmask 255.255.255.248
-
-# Routing
-up route add -net 0.0.0.0/0 gw 10.88.2.213
