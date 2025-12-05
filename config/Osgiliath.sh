cfde24f72d8280f6ceb18ec53d7a6135fa815cc1
reva <reva.erica24@gmail.com>
2025-12-05 09:05:39 +0700

    Update Osgiliath.sh

--- config/Osgiliath.sh
+++ config/Osgiliath.sh
@@ -18,3 +18,15 @@ auto eth3
 iface eth3 inet static
     address 10.88.2.217
     netmask 255.255.255.252
+
+# Routing
+up ip route add 10.88.0.0/24 via 10.88.2.218
+up ip route add 10.88.2.0/25 via 10.88.2.218
+up ip route add 10.88.2.240/30 via 10.88.2.218
+up ip route add 10.88.2.244/30 via 10.88.2.218
+up ip route add 10.88.2.248/30 via 10.88.2.218
+up ip route add 10.88.2.128/26 via 10.88.2.210
+up ip route add 10.88.2.192/29 via 10.88.2.210
+up ip route add 10.88.2.220/30 via 10.88.2.210
+up ip route add 10.88.2.224/30 via 10.88.2.210
+up ip route add 10.88.2.200/29 via 10.88.2.214
