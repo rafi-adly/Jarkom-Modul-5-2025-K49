89923d9fa22bd527ae1b0b1298a2c5f3aabc4562
reva <reva.erica24@gmail.com>
2025-12-05 09:04:07 +0700

    Update Osgiliath.sh

--- routing/Osgiliath.sh
+++ routing/Osgiliath.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+echo "[+] Configuring Osgiliath routing..."
 
-echo "[+] Setting static routes for Osgiliath..."
+# Via Minastir (eth3: .217 → .218)
+ip route add 10.88.0.0/24 via 10.88.2.218        # Elendil+Isildur
+ip route add 10.88.2.0/25 via 10.88.2.218        # Gilgalad+Cirdan
+ip route add 10.88.2.240/30 via 10.88.2.218      # Minastir-Pelargir link
+ip route add 10.88.2.244/30 via 10.88.2.218      # Palantir
+ip route add 10.88.2.248/30 via 10.88.2.218      # AnduinBanks link
 
-ip route add 10.88.0.0/24 via 10.88.2.218
-ip route add 10.88.2.0/25 via 10.88.2.218
-ip route add 10.88.2.240/30 via 10.88.2.218
-ip route add 10.88.2.244/30 via 10.88.2.218
-ip route add 10.88.2.248/30 via 10.88.2.218
-ip route add 10.88.2.236/30 via 10.88.2.218
+# Via Moria (eth2: .209 → .210)
+ip route add 10.88.2.128/26 via 10.88.2.210      # Durin
+ip route add 10.88.2.192/29 via 10.88.2.210      # Khamul
+ip route add 10.88.2.220/30 via 10.88.2.210      # Moria-Wilderland link
+ip route add 10.88.2.224/30 via 10.88.2.210      # IronHills
 
-ip route add 10.88.2.128/26 via 10.88.2.210
-ip route add 10.88.2.192/29 via 10.88.2.210
-ip route add 10.88.2.220/30 via 10.88.2.210
-ip route add 10.88.2.224/30 via 10.88.2.210
+# Via Rivendell (eth1: .213 → .214)
+ip route add 10.88.2.200/29 via 10.88.2.214     
 
-ip route add 10.88.2.200/29 via 10.88.2.214
-
-echo "[✓] Routing Osgiliath COMPLETE!"
+echo "[✓] Osgiliath routing complete!"
