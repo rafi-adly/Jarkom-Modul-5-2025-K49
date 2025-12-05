03e4649cd04eab97b735728400f3ffb5b28b4bf2
reva <reva.erica24@gmail.com>
2025-12-05 08:40:16 +0700

    Update Minastir.sh

--- config/Minastir.sh
+++ config/Minastir.sh
@@ -1,22 +1,15 @@
 auto eth0
 iface eth0 inet static
-    address 10.88.2.237
+    address 10.88.2.218    
     netmask 255.255.255.252
+    gateway 10.88.2.217
 
 auto eth1
 iface eth1 inet static
-    address 10.88.2.241
-    netmask 255.255.255.252
+    address 10.88.0.1    
+    netmask 255.255.255.0
 
 auto eth2
 iface eth2 inet static
-    address 10.88.2.218
+    address 10.88.2.241   
     netmask 255.255.255.252
-
-# Routing
-up route add -net 0.0.0.0/0 gw 10.88.2.217
-up route add -net 10.88.0.0/24 gw 10.88.2.238
-up route add -net 10.88.2.0/25 gw 10.88.2.242
-up route add -net 10.88.2.244/30 gw 10.88.2.242
-up route add -net 10.88.2.248/30 gw 10.88.2.242
-up route add -net 10.88.2.252/30 gw 10.88.2.242
