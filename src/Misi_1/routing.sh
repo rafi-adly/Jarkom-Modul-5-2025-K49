077917027e6a81a8a0c907bf03c2f1ff75f3e8f6
reva <reva.erica24@gmail.com>
2025-12-06 19:04:45 +0700

    Create routing.sh

---
+++ src/Misi_1/routing.sh
@@ -0,0 +1,20 @@
+# di Osgiliath
+
+#!/bin/bash
+
+echo "[+] Setting static routes for Osgiliath..."
+
+ip route add 10.88.0.0/24 via 10.88.2.218
+ip route add 10.88.2.0/25 via 10.88.2.218
+ip route add 10.88.2.240/30 via 10.88.2.218
+ip route add 10.88.2.244/30 via 10.88.2.218
+ip route add 10.88.2.248/30 via 10.88.2.218
+
+ip route add 10.88.2.128/26 via 10.88.2.210
+ip route add 10.88.2.192/29 via 10.88.2.210
+ip route add 10.88.2.220/30 via 10.88.2.210
+ip route add 10.88.2.224/30 via 10.88.2.210
+
+ip route add 10.88.2.200/29 via 10.88.2.214
+
+echo "[✓] Routing Osgiliath COMPLETE!"
